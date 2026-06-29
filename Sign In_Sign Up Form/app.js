aee53778f46db6af46ddc4082e12453f497f2114
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-06-29 20:16:56 +0530

    Refactor form switching logic to improve readability and maintainability

--- Sign In_Sign Up Form/app.js
+++ Sign In_Sign Up Form/app.js
@@ -1,3 +1,4 @@
+
 const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
@@ -5,15 +6,51 @@ const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
 
 
-signupBtn.onclick = () => {
-  loginForm.style.marginLeft = "-50%";
-  loginText.style.marginLeft = "-50%";
-};
-loginBtn.onclick = () => {
-  loginForm.style.marginLeft = "0%";
-  loginText.style.marginLeft = "0%";
-};
-signupLink.onclick = () => {
-  signupBtn.click();
-  return false;
-};
+function switchForm(type) {
+    if (!loginForm || !loginText) return;
+
+    const isSignup = type === "signup";
+
+    loginForm.style.marginLeft = isSignup ? "-50%" : "0%";
+    loginText.style.marginLeft = isSignup ? "-50%" : "0%";
+}
+
+
+if (signupBtn) {
+    signupBtn.addEventListener("click", () => {
+        switchForm("signup");
+    });
+}
+
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+        switchForm("login");
+    });
+}
+
+
+if (signupLink) {
+    signupLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        switchForm("signup");
+    });
+}
+
+
+// Optional: handle login submit
+if (loginForm) {
+    loginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const email = loginForm.querySelector("input[type='text']");
+        const password = loginForm.querySelector("input[type='password']");
+
+        if (!email.value || !password.value) {
+            alert("Please enter email and password");
+            return;
+        }
+
+        console.log("Login successful");
+    });
+}
